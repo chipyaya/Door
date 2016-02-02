ef5f4c11c95d2a847d3c68bd7efcd142123b93b2
chipyaya <b03902024@ntu.edu.tw>
2016-02-02 13:31:01 +0800

    add choice button, rounded terminal

--- test/js/test.js
+++ test/js/test.js
@@ -1,60 +1,62 @@
 $(document).ready(function(){
   
-  $('.tlt').textillate({
-    // the default selector to use when detecting multiple texts to animate
+  $('.tlt_q').textillate({
     selector: '.texts',
-
-    // enable looping
     loop: false,
-
-    // sets the minimum display time for each text before it is replaced
     minDisplayTime: 2000,
-
-    // sets the initial delay before starting the animation
-    // (note that depending on the in effect you may need to manually apply 
-    // visibility: hidden to the element before running this plugin)
     initialDelay: 0,
-
-    // set whether or not to automatically start animating
     autoStart: true,
-
-    // custom set of 'in' effects. This effects whether or not the 
-    // character is shown/hidden before or after an animation  
     inEffects: [],
-
-    // custom set of 'out' effects
-
-    // in animation settings
     in: {
-      // set the effect name
       effect: 'fadeInTada',
-
-      // set the delay factor applied to each consecutive character
       delayScale: 1.5,
-
-      // set the delay between each character
       delay: 80,
-
-      // set to true to animate all the characters at the same time
       sync: false,
-
-      // randomize the character sequence 
-      // (note that shuffle doesn't make sense with sync = true)
       shuffle: false,
-
-      // reverse the character sequence 
-      // (note that reverse doesn't make sense with sync = true)
       reverse: false,
-
-      // callback that executes once the animation has finished
       callback: function () {}
     },
-
-
-    // callback that executes once textillate has finished 
     callback: function () {},
+    type: 'char'
+  });
 
-    // set the type of token to animate (available types: 'char' and 'word')
+  $('.tlt_c1').textillate({
+    selector: '.texts',
+    loop: false,
+    minDisplayTime: 2000,
+    initialDelay: 700,
+    autoStart: true,
+    inEffects: [],
+    in: {
+      effect: 'fadeInTada',
+      delayScale: 1.5,
+      delay: 80,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    },
+    callback: function () {},
+    type: 'char'
+  });
+
+  $('.tlt_c2').textillate({
+    selector: '.texts',
+    loop: false,
+    minDisplayTime: 2000,
+    initialDelay: 1800,
+    autoStart: true,
+    inEffects: [],
+    in: {
+      effect: 'fadeInTada',
+      delayScale: 1.5,
+      delay: 80,
+      sync: false,
+      shuffle: false,
+      reverse: false,
+      callback: function () {}
+    },
+    callback: function () {},
     type: 'char'
   });
 });

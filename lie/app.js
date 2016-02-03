0bf2b263c1e2633c9721472d225f012fac6678e1
ivoryhuang <ivorybangle99@gmail.com>
2016-02-03 19:19:15 +0800

    set jade engine, set route, build http server

--- lie/app.js
+++ lie/app.js
@@ -5,56 +5,39 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+//var routes = require('./routes/index');
+//var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+var questions=[
+	{qnum:1, title:"Q1：你單身嗎?", ansA: "當然~我可是正港的魯蛇耶", ansB: "不是~我走魯門只是想體驗當魯蛇的感覺"},
+	{qnum:2, title:"Q2：XXXXX", ansA: "OOOOOOO", ansB: "XXXXXXXX"}
+];
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function(req, res){
+	res.render('index');
+})
+
+app.get('/Q/:qnum', function(req, res){
+	if(req.params.qnum > questions.length)
+		res.end();
+	res.render('questions',{Q:questions[parseInt(req.params.qnum)-1]});
+})
+
+app.post('/A', function(req, res){
+	console.log('req.body.ans',req.body.ans);
+})
+
+app.listen(3000, function () {
+	console.log('Example app listening on port 3000!');
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;

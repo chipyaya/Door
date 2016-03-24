7dae11bdbc462fe851bf543daeb3d6aa78713e44
ivoryhuang <ivorybangle99@gmail.com>
2016-03-24 18:46:15 +0800

    add share photo function, logout function, redirect function in sharephoto.js

--- lie/public/js/sharephoto.js
+++ lie/public/js/sharephoto.js
@@ -1,21 +1,3 @@
-function statusChangeCallback(response){
-    console.log('statusChangeCallback');
-    console.log(response);
-    if (response.status === 'connected'){
-      testAPI();
-    }
-    else if (response.status === 'not_authorized'){
-    }
-    else {
-    }
-}
-
-function checkLoginState() {
-    FB.getLoginStatus(function(response){
-    	statusChangeCallback(response);
-    });
-}
-
 window.fbAsyncInit = function(){
 	FB.init({
 		appId      : '970489853040863',
@@ -33,8 +15,54 @@ window.fbAsyncInit = function(){
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-function testAPI() {
+function checkLoginState() {
+	FB.login(function(response) {
+		if (response.status === 'connected'){
+	    	post();
+	    }
+	    else if (response.status === 'not_authorized'){
+	    }
+	    else {
+	    }
+	  // handle the response
+	}, {scope: 'publish_actions'});
+
+}
+
+var access_token;
+var user_id;
+
+function post(){
+
+	
+	//var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
+  	//var decodedPng = Base64Binary.decode(encodedPng);
+  	//console.log(decodedPng);
+	
 	FB.api('/me', function(response) {
+		user_name = response.name;
+		user_id = response.id;
+		
 		console.log('Successful login for: ' + response.name);
+		FB.api('/me/photos', 'post', {
+	        message: user_name+' is a Loser',
+	        url: 'http://media.premiumtimesng.com/wp-content/files/2015/10/snake-medicine.jpg'
+	    }, function (response) {
+
+	        if (!response || response.error) {
+	            console.log(response);
+	        } else {
+	            $('#success_notice').show();
+	        }
+
+	    });
+	});
+
+
+}
+
+function logout(){
+	FB.logout(function(response) {	
+  		window.location.href='/';
 	});
 }

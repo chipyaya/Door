56d1166c87662790d9a25ab235880be43e8ade5f
ivoryhuang <ivorybangle99@gmail.com>
2016-03-27 21:35:21 +0800

    move upload api from sharephoto.js to app.js

--- lie/public/js/sharephoto.js
+++ lie/public/js/sharephoto.js
@@ -15,69 +15,27 @@ window.fbAsyncInit = function(){
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-var access_token;
-var user_id;
-function checkLoginState() {
+function statusChangeCallback(response) {
+	
+	
 	FB.login(function(response) {
-		/*
-		if (response.status === 'connected'){
-	    	post();
-	    }
-	    else if (response.status === 'not_authorized'){
-	    }
-	    else {
-	    }
-	    */
+	
+	var access_token = response.authResponse.accessToken;
+
 	    FB.api('/me', function(response) {
-			user_name = response.name;
-			user_id = response.id;
-			
-			console.log('Successful login for: ' + response.name);
-			FB.api('/me/photos', 'post', {
-		        message: user_name+' is a Loser',
-		        url: 'http://media.premiumtimesng.com/wp-content/files/2015/10/snake-medicine.jpg'
-		    }, function (response) {
-
-		        if (!response || response.error) {
-		            console.log(response);
-		        } else {
-		            $('#success_notice').show();
-		        }
-
-		    });
+			var user_name = response.name;
+			var user_id = response.id;
+			$.post('/upload',{token:access_token, name:user_name},function(result){
+				$('#success_notice').show();
+			});
 		});
-	  // handle the response
-	}, {scope: 'publish_actions'});
-
+	}, {scope: 'publish_actions'});    
 }
 
-
-function post(){
-
-	
-	//var encodedPng = data.substring(data.indexOf(',') + 1, data.length);
-  	//var decodedPng = Base64Binary.decode(encodedPng);
-  	//console.log(decodedPng);
-	
-	FB.api('/me', function(response) {
-		user_name = response.name;
-		user_id = response.id;
-		
-		console.log('Successful login for: ' + response.name);
-		FB.api('/me/photos', 'post', {
-	        message: user_name+' is a Loser',
-	        url: 'http://media.premiumtimesng.com/wp-content/files/2015/10/snake-medicine.jpg'
-	    }, function (response) {
-
-	        if (!response || response.error) {
-	            console.log(response);
-	        } else {
-	            $('#success_notice').show();
-	        }
-
-	    });
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+	    statusChangeCallback(response);
 	});
-
 
 }
 

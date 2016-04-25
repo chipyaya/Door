e6ef3f452d6a5a54f95ab6939e9fd5ca8a7b70ce
chipyaya <b03902024@ntu.edu.tw>
2016-04-25 20:41:17 +0800

    process img: crop circle

--- lie/processImg/commands.sh
+++ lie/processImg/commands.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 # $1 = 1 (0~20%), 2(20%~40%), ...
+# level, centerX, centerY, shoulderW, leftShoulderX, leftShoulderY
 
 if [ "$1" == "1" ]; then
-	cropX=${2}-${4}/2;
-	cropY=${2}+100;		
-	x=500;y=1000;
+	cropX=${2}-${4};
+	cropY=${3}-100;		
+	x=${4};y=1000;
 fi
-convert ./public/img/raw.jpg -fuzz 15% -transparent "rgb(255,255,255)" ./public/img/person.png
-convert -crop 500x800+280+150 ./public/img/person.png ./public/img/person2.png
-convert ./public/img/bg${1}.png ./public/img/person2.png -geometry +500+1000 -composite ./public/img/composite.png
+
+# remove bg
+# convert ./public/img/raw.jpg -fuzz 15% -transparent "rgb(255,255,255)" ./public/img/person.png
+
+# mask yellow
+# convert ./public/img/raw.png -fill "rgb(251, 188)" -colorize 20%  ./public/img/person.png
+
+# crop the circle
+# convert ./public/img/person.png \( +clone -threshold -1 -negate -fill white -draw "circle ${centerX},${centerY} ${centerX}-444,${centerY} " \) -alpha off -compose copy_opacity -composite ./public/img/person.png
+convert ./public/img/raw.png \( +clone -threshold -1 -negate -fill white -draw "circle 552,560 996,560" \) -alpha off -compose copy_opacity -composite ./public/img/person.png
+	
+# composite 
+convert ./public/img/test.png ./public/img/person.png -geometry +150+-30 -composite ./public/img/composite.png
+# convert ./public/img/photo_${1}.png ./public/img/person.png -geometry +150+-30 -composite ./public/img/composite.png

4a598894804564ec1fce912b9fc92bc20d27939d
chipyaya <b03902024@ntu.edu.tw>
2016-04-26 15:12:41 +0800

    add scroll when click and revise nextq and q color

--- lie/public/js/tlt.js
+++ lie/public/js/tlt.js
@@ -1,23 +1,27 @@
 $(document).ready(function(){
-  
-  $('.tlt_q').textillate({
-    selector: '.texts',
-    loop: false,
-    minDisplayTime: 2000,
-    initialDelay: 0,
-    autoStart: true,
-    inEffects: [],
-    in: {
-      effect: 'fadeInTada',
-      delayScale: 1.5,
-      delay: 20,
-      sync: false,
-      shuffle: false,
-      reverse: false,
-      callback: function () {}
-    },
-    callback: function () {},
-    type: 'char'
-  });
-
+	tlt();
 });
+
+var tlt = function(){
+  
+	$('.tlt_q').textillate({
+		selector: '.texts',
+		loop: false,
+		initialDelay: 20,
+		autoStart: true,
+		inEffects: [],
+		in: {
+		  effect: 'fadeInTada',
+		  delayScale: 1.5,
+		  delay: 40,
+		  sync: false,
+		  shuffle: false,
+		  reverse: false,
+		  callback: function () {}
+		},
+		callback: function () {},
+		type: 'char'
+	});
+	$('.tlt_q').textillate('start')
+
+};

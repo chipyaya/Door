5baa475d8577d1840d0f7792fb3e267581b4a9c1
chipyaya <b03902024@ntu.edu.tw>
2016-04-27 15:23:24 +0800

    click dialog button to upload

--- lie/public/js/sharephoto.js
+++ lie/public/js/sharephoto.js
@@ -1,9 +1,9 @@
 window.fbAsyncInit = function(){
 	FB.init({
 		appId      : '970489853040863',
-	cookie     : true,
-	xfbml      : true,
-	version    : 'v2.5'
+		cookie     : true,
+		xfbml      : true,
+		version    : 'v2.5'
 	});
 };
 
@@ -27,53 +27,38 @@ function statusChangeCallback(response) {
 			var user_name = response.name;
 			var user_id = response.id;
 
+			//write sth msg
 			$( "#fbmessage" ).dialog({
+				resizable: false,
 				modal: true,
 				buttons: {
 					Ok: function() {
 						$( this ).dialog( "close" );
+						console.log('hi');
+						$.post('/uploadtofb',{token:access_token, name:user_name, message:$('#fbmessage input').val()},function(result){
+							$('#fbmessage').hide()
+
+							//success msg
+							$('#success_notice_fb').dialog({
+								resizable: false,
+								modal: true,
+								buttons: {
+									Ok: function() {
+										$( this ).dialog( "close" );
+									}
+								}
+							});
+
+						});
 					}
 				}
 			});
 
-			/*
-			function upload(){
-			   FB.api('/me/photos', 'post', {
-			   message: user_name+' is a Loser',
-				//url: photourl
-				url: 'http://imgur.com/1i2T4xS'
-				}, function (response) {
+		});
 
-					if (!response || response.error) {
-						console.log(response);
-					} else {
-						$('#success_notice_fb').fadeIn();
-					}
-				}); 
-				$.post('/uploadtofb',{token:access_token, name:user_name},function(result){
-					$('#success_notice').show();
-				});
-			}
-				*/	
-		$('#fbmessage button').click(function(){
-			$.post('/uploadtofb',{token:access_token, name:user_name, message:$('#fbmessage input').val()},function(result){
-		$('#fbmessage').hide()
-		$('#success_notice_fb').fadeIn();
-		/*$('#success_notice_fb').dialog({
-			height: 500,	//doesn't work!??
-			modal: true,
-			buttons: {
-				Ok: function() {
-					$( this ).dialog( "close" );
-				}
-			}
-		});*/
-	});
-});	
-
-});
-}, {scope: 'publish_actions'});    
+	}, {scope: 'publish_actions'});    
 }
+
 
 function checkLoginState() {				//call by the button "login Fb and upload img" 
 	FB.getLoginStatus(function(response) {
@@ -82,14 +67,17 @@ function checkLoginState() {				//call by the button "login Fb and upload img"
 
 }
 
+
 function restart(){                         //call by the button "END"
 	FB.logout(function(response) {});       //logout FB account
 	window.location.replace('/');           //Go back to / 
 }
 
+
 function uploadtofb(){						//unused?
 	checkLoginState();
 }
+
 
 function uploadtoimgur(){
 
@@ -99,6 +87,7 @@ function uploadtoimgur(){
 		window.location.href='/share';			//go to /share
 	});
 }
+
 
 function makeqrcode(){							//Call by the button "QRcode and download img"
 	$.get('/makeqrcode',function(data){

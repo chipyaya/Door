d9e99ff01462e1e6365c636615262b2e8b570dbc
e841018 <e841018@gmail.com>
2016-05-01 15:54:24 +0800

    Added files via upload

---
+++ lie/hardware/honestyTester/honestyTester.ino
@@ -0,0 +1,23 @@
+const float RATIO=0.25; //ratio of change of value in smoothing
+const float THR=30;     //threshold
+const char PERIOD=20;   //sampling space in milliseconds
+
+float value=1023;          //value of input(smoothed)
+float change=0;         //change of value
+unsigned long tTime=0;  //time of last trigger in milliseconds
+unsigned long HBCount=0;     //heart beat count
+
+void setup() {
+  Serial.begin(9600);
+}
+void loop(){
+  while(1){
+    change=RATIO*(analogRead(A0)-value);
+    value+=change;
+    if(millis()-tTime>300)if(change>THR){
+      tTime=millis();
+      Serial.println(++HBCount);
+    }
+    delay(PERIOD);
+  }
+}

14c6466aa1ca7224a8ce9ce34cc06677950f9756
chipyaya <b03902024@ntu.edu.tw>
2016-05-05 08:53:17 +0800

    add tv layout

--- lie/app.js
+++ lie/app.js
@@ -128,6 +128,10 @@ app.post('/uploadtofb', function(req, res){		//call by sharephoto.js
 	res.end();
 })
 
+app.get('/tv', function(req, res){
+	res.render('tv');
+})
+
 app.listen(3000, function () {
 	console.log('Example app listening on port 3000!');
 });

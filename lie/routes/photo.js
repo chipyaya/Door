8e84ad0ce57d677e9a6575b9ed53985e4714e81f
chipyaya <b03902024@ntu.edu.tw>
2016-05-07 23:23:29 +0800

    adjust processImg argument

--- lie/routes/photo.js
+++ lie/routes/photo.js
@@ -60,7 +60,7 @@ router.get('/uploadtoimgur', function(req, res){		//call by pressing the button 
 
 	//readFile
 	fs.readFile('./kinect_code/coordinate.txt', 'utf8', function(err,data){
-		strarr = data.split("\n");
+		var strarr = data.split("\n");
 		console.log(strarr);
 		var filename = parseInt(strarr[0]);
 		var centerX = parseInt(strarr[1]);

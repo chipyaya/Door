4ac04961822427193e8480b5657a05b7100ae67f
chipyaya <b03902024@ntu.edu.tw>
2016-05-15 17:26:30 +0800

    change convert path, enlarge loading time

--- lie/test.bat
+++ lie/test.bat
@@ -1,19 +1,16 @@
-
 SET level=%1
 SET filename=%2
 SET centerX=%3
 SET centerY=%4
-SET /a anotherX=centerX-200
+SET /a anotherX=centerX-430
 
-SET convert_cmd="C:\Program Files\ImageMagick-7.0.1-Q16-HDRI\convert.exe"
+SET convert_cmd="C:\Windows\System32\ImageMagick-7.0.1-1-portable-Q16-x64\convert.exe"
 SET img_path=.\kinect_code\NTUAF-Recognize\NTUAF-Recognize\images\
 SET jpeg_path=%img_path%%filename%.jpeg
 SET png_path=%img_path%%filename%.png
 SET person_path=%img_path%person.png
 SET cir="circle %centerX%,%centerY% %anotherX%,%centerY%"
 %convert_cmd% %jpeg_path% %png_path%
-
-:: %convert_cmd% %png_path% ( +clone -threshold -1 -negate -fill white -draw "circle %centerX%,%centerY% %anotherX%,%centerY% " ) -alpha off -compose copy_opacity -composite person_path.png
 
 %convert_cmd% -size 1290x1080 xc:none -fill %png_path% -draw %cir% %person_path% 
 

46a8232b0eb7c84d84ce404a6de230ec319b0de0
chipyaya <b03902024@ntu.edu.tw>
2016-05-15 22:44:24 +0800

    qr + kinect

--- lie/routes/photo.js
+++ lie/routes/photo.js
@@ -1,6 +1,4 @@
-var express = require('express');
-var router = express.Router();
-var https = require('https');
+var express = require('express'); var router = express.Router(); var https = require('https');
 var bodyParser = require('body-parser');
 var FB = require('fb');
 var fs = require('fs');
@@ -21,6 +19,7 @@ router.get('/', function(req, res){
 })
 
 var lastpulse;
+var photourl;
 
 fs.readFile('hardware/heartBeat/heartBeat.txt', 'utf8', function(err,data){
 	lastpulse = parseInt(data);
@@ -153,7 +152,7 @@ router.get('/uploadtoimgur', function(req, res){		//call by pressing the button 
 		var albumId = 'fGZi1';
 		imgur.uploadFile('public/img/composite.png', albumId)	//upload to imgur
 			.then(function (json) {
-				var photourl = json.data.link;
+				photourl = json.data.link;
 				console.log(photourl);
 				//res.redirect('/share');
 				res.json(json);
@@ -171,7 +170,7 @@ router.get('/share', function(req, res){			//call by pressing the button in ques
 });
 
 router.get('/makeqrcode', function(req,res){		//call by pressing the button in sharephoto.jade
-	var qrcode = qr.image('http://i.imgur.com/96ySytj.png', { type: 'svg' });	
+	var qrcode = qr.image(photourl, { type: 'svg' });	
 		res.type('svg');
 	qrcode.pipe(res);
 });
